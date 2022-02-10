2249b9870cdbb47145e49e465f0aaadc916008c3
Dominic Tremblay <dominictremblay24@gmail.com>
2022-02-10 14:14:50 -0500

    adding the client

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: '/api/users',
+    })
+      .then((result) => {
+        console.log(result.data);
+        setUsers(result.data);
+      })
+      .catch((err) => console.log(`Error: err.message`));
+  }, []);
+
+  const userList = users && users.map(user => <li key={user.id}> {user.first_name} {user.last_name} {user.email} </li>)
+
+  return <div className="App">
+
+  <ul>
+    {userList}
+  </ul>
+
+  </div>;
+}
+
+export default App;
